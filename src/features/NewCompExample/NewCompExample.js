f43525e9a1ae57eee49dd1baf8fc6cad3bfa7747
Hugo Boehm <hugo.boehm98@gmail.com>
2020-10-15 10:57:47 +0200

    Clean up

---
+++ src/features/NewCompExample/NewCompExample.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, Box } from 'grommet';
+
+export const NewCompExample = () => {
+    return(
+        <Box justify="center" align="center">
+            <Text>Exemple import New Componant </Text>
+        </Box>
+    )
+};
